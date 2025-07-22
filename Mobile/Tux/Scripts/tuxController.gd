20da31cc737273922f55b375b7497777e23b1105
Mikey9124x <133249018+Mikey9124x@users.noreply.github.com>
2025-07-21 23:57:00 -0500

    Other Half

--- Mobile/Tux/Scripts/tuxController.gd
+++ Mobile/Tux/Scripts/tuxController.gd
@@ -1,8 +1,17 @@
 extends CharacterBody3D
+var shoot = true
+var health = 100
+var coins = 0
+var grav = 1
+signal attacked()
+var over = preload("res://GameOver.tscn").instantiate()
+func _on_timer_timeout() -> void:
+	shoot = true
+
 #Speed of tux
-const SPEED = 20.0
+const SPEED = 10.0
 #How high tux jumps
-const JUMP_VELOCITY = 6
+const JUMP_VELOCITY = 10
 var scene = preload("res://snowball.tscn")
 func _ready() -> void:
 #Captures the mouse. Move somewhere else?
@@ -15,19 +24,23 @@ func _input(event):
 func _physics_process(delta: float) -> void:
 	#Gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * grav
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	#Handle Crouch
 	if Input.is_action_pressed("crouch") :
 		scale.y=0.5
+		grav = 10
 	else:
 		scale.y=1
-	if Input.is_action_just_pressed("e"):
+		grav = 1
+	if Input.is_action_just_pressed("e") && shoot == true:
 		var sun =scene.instantiate()
-	#	sun.rotation = rotation
+#		sun.rotation = rotation
 		add_child(sun)
+		shoot=false
+		emit_signal("attacked")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -40,3 +53,20 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	health=health -33.333
+	if health<0.01:
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		get_tree().root.add_child(over)
+		get_node("/root/Main").queue_free()
+
+
+func _on_area_3d_2_area_entered(area: Area3D) -> void:
+	coins = coins + 1
+	get_node("Coin").play()
+	if coins==100:
+		health = health + 33.3
+		coins=0
+		
---
+++ Mobile/Tux/timerAttack.gd
@@ -0,0 +1,15 @@
+extends Timer
+var shoot = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+#func _on_character_body_3d_child_entered_tree(node: Node) -> void:
+	#start(1)
+
+
+
+
+func _on_character_body_3d_attacked() -> void:
+	start(1)
